6fbfa850dfcd4e013374c9ab0f19c7d8898894cf
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-08-16 18:13:03 +0300

    center login at screen

--- views/login.go
+++ views/login.go
@@ -2,6 +2,7 @@ package views
 
 import (
 	"desktop-app-template/auth"
+	"desktop-app-template/utils"
 	"log"
 
 	"fyne.io/fyne/v2"
@@ -11,35 +12,40 @@ import (
 )
 
 func LoginView(window fyne.Window, showDashboard func()) *fyne.Container {
-    usernameEntry := widget.NewEntry()
-    usernameEntry.SetPlaceHolder("Username")
+	usernameEntry := widget.NewEntry()
+	usernameEntry.SetPlaceHolder("Username")
 
-    passwordEntry := widget.NewPasswordEntry()
-    passwordEntry.SetPlaceHolder("Password")
+	passwordEntry := widget.NewPasswordEntry()
+	passwordEntry.SetPlaceHolder("Password")
 
-    loginButton := widget.NewButton("Login", func() {
-        username := usernameEntry.Text
-        password := passwordEntry.Text
+	loginButton := widget.NewButton("Login", func() {
+		username := usernameEntry.Text
+		password := passwordEntry.Text
 
-        user, err := auth.Login(username, password)
-        if err != nil {
-            log.Println("Failed to login:", err)
-            dialog.ShowError(err, window)
-        } else {
-            log.Println("User logged in:", user.Username)
-            dialog.ShowInformation("Login Successful", "Welcome, "+user.Username, window)
-            showDashboard()
-        }
-    })
+		user, err := auth.Login(username, password)
+		if err != nil {
+			log.Println("Failed to login:", err)
+			dialog.ShowError(err, window)
+		} else {
+			log.Println("User logged in:", user.Username)
+			dialog.ShowInformation("Login Successful", "Welcome, "+user.Username, window)
+			showDashboard()
+		}
+	})
 
-    registerButton := widget.NewButton("Register", func() {
-        window.SetContent(RegisterView(window, showDashboard))
-    })
+	registerButton := widget.NewButton("Register", func() {
+		window.SetContent(RegisterView(window, showDashboard))
+	})
 
-    return container.NewVBox(
-        usernameEntry,
-        passwordEntry,
-        loginButton,
-        registerButton,
-    )
+	form := container.NewVBox(
+		usernameEntry,
+		passwordEntry,
+		loginButton,
+		registerButton,
+	)
+
+	centeredForm := utils.NewFixedWidthCenter(form, 300) // Set width to 300
+
+	return container.NewCenter(centeredForm)
+
 }
