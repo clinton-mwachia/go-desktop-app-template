a2b563f94db96a85ae69e765622aaa6869e8e77b
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-08-27 12:06:31 +0300

    moved notifications view to its file

--- views/header.go
+++ views/header.go
@@ -2,12 +2,9 @@ package views
 
 import (
 	"desktop-app-template/models"
-	"desktop-app-template/utils"
-	"strconv"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
@@ -16,6 +13,7 @@ import (
 var (
 	notificationCountLabel *widget.Label
 	notificationIcon       *widget.Button
+	settingsIcon           *widget.Button
 	notifications          []models.Notification
 )
 
@@ -26,6 +24,11 @@ func Header(window fyne.Window) *fyne.Container {
 		showNotifications(window)
 	})
 
+	// settings icon
+	settingsIcon = widget.NewButtonWithIcon("", theme.SettingsIcon(), func() {
+		showSettings(window)
+	})
+
 	// Set initial count
 	updateNotificationCount(window)
 
@@ -33,68 +36,10 @@ func Header(window fyne.Window) *fyne.Container {
 	header := container.NewHBox(
 		widget.NewLabel("Go Template"),
 		layout.NewSpacer(),
+		settingsIcon,
 		notificationIcon,
 		notificationCountLabel,
 	)
 
 	return header
-}
-
-func showNotifications(window fyne.Window) {
-	// Fetch notifications from the database
-	userID := utils.CurrentUserID
-	notifications = utils.FetchNotifications(userID, window)
-
-	if len(notifications) == 0 {
-		dialog.ShowInformation("Notifications", "No notifications found.", window)
-		return
-	}
-
-	// Create a list to display notifications
-	list := widget.NewList(
-		func() int { return len(notifications) },
-		func() fyne.CanvasObject {
-			return widget.NewLabel("")
-		},
-		func(id widget.ListItemID, obj fyne.CanvasObject) {
-			obj.(*widget.Label).SetText(notifications[id].Message)
-		},
-	)
-
-	// Create buttons for clearing and marking notifications
-	markAsReadButton := widget.NewButton("Mark All as Read", func() {
-		utils.MarkNotificationsAsRead(userID, window)
-		updateNotificationCount(window)
-		notificationIcon.Refresh()
-		dialog.ShowInformation("Notifications", "All notifications marked as read.", window)
-	})
-
-	clearButton := widget.NewButton("Clear All", func() {
-		utils.ClearNotifications(userID, window)
-		updateNotificationCount(window)
-		notificationIcon.Refresh()
-		notifications = utils.FetchNotifications(userID, window)
-		list.Refresh() // Refresh the list widget to update UI
-		dialog.ShowInformation("Notifications", "All notifications cleared.", window)
-	})
-
-	// Create a scrollable container for the list
-	scrollableList := container.NewVScroll(list)
-	scrollableList.SetMinSize(fyne.NewSize(400, 250))
-
-	// Create a container for the list and buttons
-	content := container.NewVBox(
-		scrollableList,
-		container.NewHBox(markAsReadButton, clearButton),
-	)
-
-	// Show notifications in a new window or dialog
-	dialog.ShowCustom("Notifications", "Close", content, window)
-}
-
-func updateNotificationCount(window fyne.Window) {
-	userID := utils.CurrentUserID
-	unreadCount := utils.GetUnreadNotificationsCount(userID, window)
-	notificationCountLabel.SetText(strconv.Itoa(unreadCount))
-	notificationIcon.Refresh()
 }
