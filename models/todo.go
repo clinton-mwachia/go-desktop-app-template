12df30e103306d9569e8f7646478e95388420547
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-08-29 13:14:26 +0300

    get statistics on todos

--- models/todo.go
+++ models/todo.go
@@ -1,11 +1,19 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Todo struct {
-    ID       primitive.ObjectID `bson:"_id,omitempty"`
-    UserID   primitive.ObjectID `bson:"user_id"`
-    Title    string             `bson:"title"`
-    Content  string             `bson:"content"`
-    Done     bool               `bson:"done"`
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	UserID    primitive.ObjectID `bson:"user_id"`
+	Title     string             `bson:"title"`
+	Content   string             `bson:"content"`
+	Done      bool               `bson:"done"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
 }
+
+// time.Now().Format("2006-01-02 15:04:05")
