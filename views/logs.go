5eb4a2145970e026669fa4707b924756c1554395
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-09-04 14:43:59 +0300

    added logs view

---
+++ views/logs.go
@@ -0,0 +1,254 @@
+package views
+
+import (
+	"desktop-app-template/models"
+	"desktop-app-template/utils"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"math"
+	"os"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+const (
+	logsPerPage = 5
+)
+
+func LogsView(window fyne.Window) fyne.CanvasObject {
+	var logList *widget.List
+	var logs []models.Log
+	var currentPage int = 1
+	var totalLogs int64 = 0
+	var pageLabel *widget.Label
+	var prevButton, nextButton *widget.Button
+	var searchResults []models.Log
+	var searchEntry *widget.Entry
+	var noResultsLabel *widget.Label
+
+	header := Header(window)
+
+	// Load logs for the specified page
+	loadLogs := func(page int) {
+		// Check if search is active
+		if searchEntry.Text != "" {
+			// Use filtered logs when a search query is active
+			logs = searchResults
+			totalLogs = int64(len(logs))
+		} else {
+			// Use all logs for normal pagination
+			logs = utils.GetLogsPaginated(page, logsPerPage, window)
+			totalLogs = utils.CountLogs(window)
+		}
+
+		logList.Refresh()
+
+		// Enable or disable pagination buttons based on the current page and total pages
+		totalPages := int(math.Ceil(float64(totalLogs) / float64(logsPerPage)))
+
+		// Update page label
+		pageLabel.SetText(fmt.Sprintf("Page %d of %d", currentPage, totalPages))
+
+		prevButton.Disable()
+		nextButton.Disable()
+		if currentPage > 1 {
+			prevButton.Enable()
+		}
+		if currentPage < totalPages {
+			nextButton.Enable()
+		}
+	}
+
+	// Update visibility of no results label
+	updateNoResultsLabel := func() {
+		if len(logs) == 0 {
+			noResultsLabel.Show()
+		} else {
+			noResultsLabel.Hide()
+		}
+	}
+
+	updateLogList := func() {
+		loadLogs(currentPage)
+		updateNoResultsLabel()
+	}
+
+	// Header Row with Titles
+	titleRow := container.NewHBox(
+		widget.NewLabelWithStyle("User", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+		layout.NewSpacer(),
+		widget.NewLabelWithStyle("TimeStamp", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+		layout.NewSpacer(),
+		widget.NewLabelWithStyle("Details", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+		layout.NewSpacer(),
+		widget.NewLabelWithStyle("CreatedAt", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+		layout.NewSpacer(),
+		widget.NewLabelWithStyle("UpdatedAt", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+	)
+
+	// Create the logs list
+	logList = widget.NewList(
+		func() int {
+			return len(logs)
+		},
+		func() fyne.CanvasObject {
+			// user label
+			detailLabel := widget.NewLabelWithStyle("", fyne.TextAlignLeading, fyne.TextStyle{})
+			detailLabel.Truncation = fyne.TextTruncation(fyne.TextTruncateEllipsis)
+
+			// content label
+			timeStampLabel := widget.NewLabelWithStyle("", fyne.TextAlignLeading, fyne.TextStyle{})
+
+			// time
+			createdAtLabel := widget.NewLabelWithStyle("", fyne.TextAlignLeading, fyne.TextStyle{})
+			updatedAtLabel := widget.NewLabelWithStyle("", fyne.TextAlignLeading, fyne.TextStyle{})
+
+			row := container.NewGridWithColumns(5,
+				detailLabel,
+				timeStampLabel,
+				createdAtLabel,
+				updatedAtLabel,
+			)
+			return row
+		},
+		func(id widget.ListItemID, obj fyne.CanvasObject) {
+			log := logs[id]
+			row := obj.(*fyne.Container)
+
+			// Retrieve the components in the row
+			detailLabel := row.Objects[0].(*widget.Label)
+			timeStampLabel := row.Objects[1].(*widget.Label)
+			createdAtLabel := row.Objects[2].(*widget.Label)
+			updatedAtLabel := row.Objects[3].(*widget.Label)
+
+			detailLabel.Wrapping = fyne.TextWrapWord
+			detailLabel.SetText(log.Details)
+			timeStampLabel.SetText(log.Timestamp.Format("2006-01-02 15:04:05"))
+			createdAtLabel.SetText(log.CreatedAt.Format("2006-01-02 15:04:05")) // convert time to string
+			updatedAtLabel.SetText(log.UpdatedAt.Format("2006-01-02 15:04:05")) // convert time to string
+
+		},
+	)
+
+	// Pagination controls
+	pagination := container.NewHBox()
+	prevButton = widget.NewButton("Previous", func() {
+		if currentPage > 1 {
+			currentPage--
+			updateLogList()
+		}
+	})
+	nextButton = widget.NewButton("Next", func() {
+		if int(math.Ceil(float64(totalLogs)/float64(logsPerPage))) > currentPage {
+			currentPage++
+			updateLogList()
+		}
+	})
+
+	// Initialize page label
+	pageLabel = widget.NewLabel(fmt.Sprintf("Page %d of %d", currentPage, int(math.Ceil(float64(totalLogs)/float64(logsPerPage)))))
+
+	// Add buttons and label to the pagination container
+	pagination.Add(prevButton)
+	pagination.Add(pageLabel)
+	pagination.Add(nextButton)
+
+	// Center the pagination controls
+	pagination = container.NewCenter(pagination)
+
+	// Search functionality
+	searchEntry = widget.NewEntry()
+	searchEntry.SetPlaceHolder("Search Logs...")
+	searchButton := widget.NewButton("Search Logs", func() {
+		searchText := searchEntry.Text
+		if searchText != "" {
+			searchResults = utils.SearchLogs(searchText, window)
+			updateNoResultsLabel()
+			currentPage = 1 // Reset to first page of search results
+			updateLogList()
+		} else {
+			// If search is cleared, reset the pagination and todo list
+			searchResults = nil
+			currentPage = 1
+			updateLogList()
+		}
+	})
+
+	// Define functions for exporting data
+	exportToCSV := widget.NewButton("export to csv", func() {
+		logs := utils.GetAllLogs(window)
+		file, err := os.Create("logs.csv")
+		if err != nil {
+			dialog.ShowError(err, window)
+			return
+		}
+		defer file.Close()
+
+		writer := csv.NewWriter(file)
+		defer writer.Flush()
+
+		// Write header
+		writer.Write([]string{"ID", "UserID", "Details", "Timestamp", "CreatedAt", "UpdatedAt"})
+
+		// Write todo data
+		for _, log := range logs {
+			writer.Write([]string{
+				log.ID.Hex(),
+				log.UserID.Hex(),
+				log.Details,
+				log.Timestamp.Format("2006-01-02 15:04:05"),
+				log.CreatedAt.Format("2006-01-02 15:04:05"),
+				log.UpdatedAt.Format("2006-01-02 15:04:05"),
+			})
+		}
+
+		dialog.ShowInformation("Export Successful", "logs have been exported to logs.csv", window)
+	})
+
+	exportToJSON := widget.NewButton("export to json", func() {
+		logs := utils.GetAllLogs(window)
+		file, err := os.Create("logs.json")
+		if err != nil {
+			dialog.ShowError(err, window)
+			return
+		}
+		defer file.Close()
+
+		encoder := json.NewEncoder(file)
+		encoder.SetIndent("", "  ")
+		err = encoder.Encode(logs)
+		if err != nil {
+			dialog.ShowError(err, window)
+			return
+		}
+
+		dialog.ShowInformation("Export Successful", "logs have been exported to logs.json", window)
+	})
+
+	// the search entry and bulk upload button
+	searchContainer := container.New(layout.NewGridLayout(2), searchEntry, searchButton)
+
+	// No results label
+	noResultsLabel = widget.NewLabel("No results found")
+	noResultsLabel.Hide() // Hide by default
+
+	// Load the initial set of todos
+	updateLogList()
+
+	// grid for the add log and export logs button
+	exportButtonContainer := container.New(layout.NewGridLayout(2), exportToCSV, exportToJSON)
+
+	// Define the container for the list with pagination controls
+	listContainer := container.NewBorder(titleRow, nil, nil, nil, logList, noResultsLabel)
+
+	listWrapper := container.NewBorder(exportButtonContainer, pagination, nil, nil, listContainer)
+
+	// Return the final container with all elements
+	return container.NewBorder(header, nil, nil, nil, container.NewBorder(searchContainer, nil, nil, nil, listWrapper))
+
+}
