c8353194ce03319f321bdf3945ff40c585a1963f
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-09-04 16:06:36 +0300

    added log on successive login

--- views/login.go
+++ views/login.go
@@ -2,13 +2,17 @@ package views
 
 import (
 	"desktop-app-template/auth"
+	"desktop-app-template/models"
 	"desktop-app-template/utils"
+	"fmt"
 	"log"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func LoginView(window fyne.Window, showDashboard func()) *fyne.Container {
@@ -25,9 +29,23 @@ func LoginView(window fyne.Window, showDashboard func()) *fyne.Container {
 		user, err := auth.Login(username, password)
 		if err != nil {
 			log.Println("Failed to login:", err)
+			fmt.Println(err)
 			dialog.ShowError(err, window)
 		} else {
 			log.Println("User logged in:", user.Username)
+			parsedTime, err := time.Parse("2006-01-02 15:04:05", time.Now().Format("2006-01-02 15:04:05"))
+
+			if err != nil {
+				dialog.ShowError(err, window)
+				return
+			}
+			myLog := models.Log{
+				ID:        primitive.NewObjectID(),
+				Timestamp: parsedTime,
+				Details:   user.Username + " logged in",
+				Status:    "SUCCESS",
+			}
+			utils.AddLog(myLog, window)
 			utils.CurrentUserID = user.ID
 			dialog.ShowInformation("Login Successful", "Welcome, "+user.Username, window)
 			showDashboard()
