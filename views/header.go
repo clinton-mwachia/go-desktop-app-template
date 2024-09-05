55f63ea2c50634bc2c094f67a3fe5a1f7de8ebf7
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-09-05 12:53:08 +0300

    save darkmode to file

--- views/header.go
+++ views/header.go
@@ -2,6 +2,9 @@ package views
 
 import (
 	"desktop-app-template/models"
+	"encoding/json"
+	"log"
+	"os"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
@@ -23,6 +26,49 @@ type AppSettings struct {
 	IsDarkMode bool `json:"is_dark_mode"`
 }
 
+const settingsFilePath = "settings.json"
+
+// LoadSettings loads the app settings from a JSON file
+func LoadSettings() (*AppSettings, error) {
+	// Check if the settings file exists
+	if _, err := os.Stat(settingsFilePath); os.IsNotExist(err) {
+		// If it doesn't exist, return default settings
+		return &AppSettings{IsDarkMode: false}, nil
+	}
+
+	// Read the settings file
+	fileBytes, err := os.ReadFile(settingsFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal the JSON data into the AppSettings struct
+	var settings AppSettings
+	err = json.Unmarshal(fileBytes, &settings)
+	if err != nil {
+		return nil, err
+	}
+
+	return &settings, nil
+}
+
+// SaveSettings saves the app settings to a JSON file
+func SaveSettings(settings *AppSettings) error {
+	// Marshal the settings struct into JSON format
+	fileBytes, err := json.Marshal(settings)
+	if err != nil {
+		return err
+	}
+
+	// Write the JSON data to the settings file
+	err = os.WriteFile(settingsFilePath, fileBytes, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // Variable to track current theme mode
 var isDarkMode bool = false
 
@@ -37,11 +83,17 @@ func applyTheme() {
 	}
 }
 
-// Function to toggle between light and dark mode
-
+// Function to toggle between light and dark mode and save the settings
 func toggleTheme() {
 	isDarkMode = !isDarkMode
 	applyTheme()
+
+	// Save the current theme setting
+	settings := &AppSettings{IsDarkMode: isDarkMode}
+	err := SaveSettings(settings)
+	if err != nil {
+		log.Println("Error saving settings:", err)
+	}
 }
 
 func Header(window fyne.Window) *fyne.Container {
